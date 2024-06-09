a6ee7c0834e16ae8ee6e0635172e2249160ea8c6
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2024-06-08 23:24:33 -0600

    Hola mundo

---
+++ introduccion/hola-mundo.rb
@@ -0,0 +1,9 @@
+# Ruby viene de Python y Perl
+# El japones Yukiro Matsumoto entre 1993 y 1995 lo publico 
+
+# Las variables no llevan ni var ni string ni nada
+# Una variable puede ser int y puede pasar a hacer string o double
+
+# Para ejecutar
+# D:\Repertorio\Servidor\aContinuar\introduccion>ruby hola-mundo.rb
+puts "Hola mundo"
