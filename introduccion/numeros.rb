1f0812055fb1426cb47aa21fb352819d8a1aeae3
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2024-06-09 00:04:45 -0600

    Numeros

---
+++ introduccion/numeros.rb
@@ -0,0 +1,15 @@
+# irb
+# interactive Ruby command prompt
+
+# Lo de siempre, como todos los lenguajes de programacion
+# % siempre el residuo
+# 2 ** 2 esto es potencia 
+puts 2 + 2
+
+# ruby a los enteros los clasifica como fixnum
+# Y a los flotantes como float
+
+# Decimales
+# Numeros de puntos flotantes
+puts medida = 30.5
+puts medida + 2.4
