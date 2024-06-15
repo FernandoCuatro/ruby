7b1b73fa9950f2405fa10b9bec3a03143d9acb7b
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2024-06-14 23:50:47 -0600

    Operadores logicos

--- introduccion/operadores_logicos.rb
+++ introduccion/operadores_logicos.rb
@@ -1,16 +1,26 @@
 # && 	AND 	AMBAS CONDICIONESS SE CUMPLEN
 # || 	OR 		UNA O OTRA SE CUMPLE
 
-calificacion = 1
-
-if calificacion == 0 || calificacion == 1
-	print "Estas hundido"
-elsif calificacion >= 2 && calificacion <= 6
-	print "Tenias que estudiar mas"
-elsif calificacion == 7 || calificacion == 8
-	print "Es buena calificacion"
-elsif calificacion == 9
-	print "Buen trabajo"
-else 
-	print "Felicidades"
+# Definimos el argumento de calificación
+def evaluar( calificacion )
+	if calificacion == 0 || calificacion == 1
+		print "Estas hundido"
+	elsif calificacion >= 2 && calificacion <= 6
+		print "Tenias que estudiar mas"
+	elsif calificacion == 7 || calificacion == 8
+		print "Es buena calificación"
+	elsif calificacion == 9
+		print "Buen trabajo"
+	elsif calificacion == 10
+		print "Felicidades"
+	else 
+		print "calificación no valida"
+	end
 end
+
+print "¿Cual es tu calificacion? "
+# Obtenemos la calificacion del usuario
+calificacion = gets.to_i # gets obtiene una cadena de texto. se tiene que convertir
+
+# Pasamos el argumento
+evaluar( calificacion )
