fc82d4e40174ea11e426bfdb52888e3aa7d1bf18
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2024-07-10 23:46:14 -0600

    Añadiendo Elementos

---
+++ 2-estructura-datos/aniandiendo-elementos.rb
@@ -0,0 +1,15 @@
+videojuegos = ["zelda","mario"]
+
+# Una opcion
+# Para agregar a un array existente
+videojuegos << "Super Smash"
+
+# metodo de los arreglos
+videojuegos.push("Mario Kart")
+
+# Si quieremos agregar al inicio
+videojuegos.unshift("Elemento 1")
+
+# Para agregar dos elementos
+# Podemos usar la figura que le estamos sumando otro arreglo
+videojuegos += ["FIFA", "PES"]
