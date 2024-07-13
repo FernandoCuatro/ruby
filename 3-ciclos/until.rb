20e8444a726ec8da6a96ad808ede6e9178c12a7c
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2024-07-12 23:17:57 -0600

    Until

---
+++ 3-ciclos/until.rb
@@ -0,0 +1,12 @@
+# Creamos un arreglo
+# forma 1
+arreglo_videojuegos = ["FIFA", "ETS", "Mario Kart"]
+puts arreglo_videojuegos
+
+# arreglo vacio
+arreglo_vacio = []
+
+# forma 2
+arreglo_nuevo = Array.new
+# Se envia el tamaño del objeto
+# arreglo_nuevo = Array.new()
