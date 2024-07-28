cb78bc6f9cfb1b92353e5ff3e80a2038660d73f9
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2024-07-27 18:41:31 -0600

    until

---
+++ 3-ciclos/until.rb
@@ -0,0 +1,9 @@
+# a diferencia del ciclo while se va a ejecutar 
+# miestras la condicion que tenga enfrente sea false
+respuesta = ""
+
+until respuesta == "n"
+	puts "Estoy jugando"
+	print "Moriste, ¿Quieres seguir jugando? s/n "
+	respuesta = gets.chomp
+end
