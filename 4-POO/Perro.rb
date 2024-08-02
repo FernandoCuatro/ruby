12b0984e11152f47a9abf229249830e56014431e
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2024-08-01 23:57:14 -0600

    Attributes

--- 4-POO/Perro.rb
+++ 4-POO/Perro.rb
@@ -1,5 +1,8 @@
 # Definimos la clase
 class Perro
+	# Esto es un tipo getter and setter
+	attr_reader :raza, :color, :edad
+
 	# Variables de instancia
 	def initialize(raza, color, edad)
 		# Es como si hicieramos un this, solo que va con la @
@@ -10,17 +13,19 @@ class Perro
 	end
 
 	# Atributos
-	def raza 
-		@raza
-	end
 
-	def color
-		@color
-	end
+	# Con lo de la linea 4 no necestamos este codigo
+	# def raza 
+	# 	@raza
+	# end
 
-	def edad
-		@edad
-	end
+	# def color
+	# 	@color
+	# end
+
+	# def edad
+	# 	@edad
+	# end
 end
 
 # Nueva instancia
