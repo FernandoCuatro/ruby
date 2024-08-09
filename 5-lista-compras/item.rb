83128bdee721e77b02b4a13790ba5b452325ba48
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2024-08-08 23:49:34 -0600

    La clase Item

--- 5-lista-compras/item.rb
+++ 5-lista-compras/item.rb
@@ -1,1 +1,20 @@
-puts "Empezamos"
+class item
+	attr_writter :text, :checked
+
+	# Metodo para inializar
+	def initialize(text = "")
+		@text = text
+		@checked = false 
+	end
+
+	# Definimos el metodo to_s
+	def to_s
+		# Si esta con check es que ya lo compramos
+		if @checked
+			"[ x ]" + @text
+		else
+			"[   ]" + @text
+		end
+	end
+
+end
