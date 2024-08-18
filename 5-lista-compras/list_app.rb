162ffdb51d7d6e9b390bee0fd4f3a93158dcc997
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2024-08-18 12:58:38 -0600

    Mejorando la interfaz de la Aplicacion

--- 5-lista-compras/list_app.rb
+++ 5-lista-compras/list_app.rb
@@ -29,24 +29,83 @@ class ListApp
 			puts "r - Remover un articulo"
 			puts "v - Mostrar todos los artículos"
 			puts "m - Marcar un articulo"
-			puts "b - Borrar todos losa artículos"
+			puts "b - Borrar todos los artículos"
 			puts "s - Salir de la Aplicación"
 
 			# Obtenemos la decisión del usuario
-			puts ""
 			print "Respuesta: "
 			input = gets.chomp
 
 			# Evaluamos
 			case input
+			when 'a'
+				puts ""
+				puts "-- Agrega un Articulo --"
+
+				print "Nombre Articulo: "
+				articulo = gets.chomp
+
+				@list.add_item(articulo)
+				print "#{articulo} Agregado con Éxito"
+				puts ""
+				puts "----- xxxxx -----"
+				puts ""
+
+			when 'r'
+				puts ""
+				puts "-- Remover un Articulo --"
+				@list.show_all
+
+				print "Numero Articulo a Eliminar: "
+				articulo = gets.to_i
+
+				@list.remove_item(articulo)
+				print "Articulo #{articulo} Removido"
+				puts ""
+				puts "----- xxxxx -----"
+				puts ""
+
+			when 'v'
+				puts ""
+				puts "-- Lista Artículos --"
+				@list.show_all
+				puts "----- xxxxx -----"
+				puts ""
+
+			when 'm'
+				puts ""
+				puts "-- Completar Tarea --"
+				@list.show_all
+
+				print "Numero Articulo a Completar: "
+				articulo = gets.to_i
+
+				@list.check_item(articulo)
+				print "Tarea Completada"
+				puts ""
+				puts "----- xxxxx -----"
+				puts ""
+
+			when 'b'
+				puts ""
+				puts "-- Borrar todos los artículos --"
+
+				print "¿Estas seguro? Esta tarea no se puede revertir: s/n "
+				respuesta = gets.chomp
+
+				if respuesta == "s"
+					@list.remove_all
+					print "Se Removieron todos los Artículos"
+					puts ""
+				end
+
+				puts "----- xxxxx -----"
+				puts ""
+			
 			when 's'
 				break
+
 			end
-
-
-
-
-
 		end
 
 		puts "Gracias por utilizar nuestra Aplicación"
