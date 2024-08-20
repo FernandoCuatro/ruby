c160c16b154693569b89898f01b48cdf49944cd7
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2024-08-19 23:50:17 -0600

    Bloques en Ruby

---
+++ 6-bloques/blocks.rb
@@ -0,0 +1,6 @@
+# Los bloques de codigo funcionan como piezas de sintaxis que podemos utilziar para llamarlas en funcion o variables
+
+# Los parametros van entre 
+7.times do |time, nombre, apellido|
+	puts "Esto es parte de un bloque de codigo #{time}"
+end 
